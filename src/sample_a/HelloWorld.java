1308e8648fb85df20b8adba208f14eac1d179ead
umotom <63347661+umotom@users.noreply.github.com>
2022-01-08 18:08:13 +0900

    first commit

---
+++ src/sample_a/HelloWorld.java
@@ -0,0 +1,8 @@
+package sample_a;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("sample_a");
+	}
+}
---
+++ src/sample_b/HelloWorld.java
@@ -0,0 +1,8 @@
+package sample_b;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("sample_b");
+	}
+}
