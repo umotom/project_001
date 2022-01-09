9bc725bf3231c0d8b5cbee34149a66b1b45f1773
umotom <63347661+umotom@users.noreply.github.com>
2022-01-09 17:36:48 +0900

    from git bash first commit

--- src/sample_a/HelloWorld.java
+++ src/sample_a/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("sample_a");
+		System.out.println("change from gitbush");
 	}
 }
