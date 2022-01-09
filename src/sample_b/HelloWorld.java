c4a175997d2f626d3b7ea3d0e859e1b17c8330c7
umotom <63347661+umotom@users.noreply.github.com>
2022-01-09 18:22:29 +0900

    from git bash second commit

--- src/sample_b/HelloWorld.java
+++ src/sample_b/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("sample_b");
+		System.out.println("change from bit bash second commit");
 	}
 }
